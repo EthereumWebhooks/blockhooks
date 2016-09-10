75ec9f5467cc7dc695cdf778a8f5142c872d884a
Nick Johnson <arachnid@notdot.net>
2016-09-10 19:05:07 +0100

    Restructure to use flexible python runtime

--- models.py
+++ models.py
@@ -1,11 +1,18 @@
+from ethereum import abi
 from google.appengine.ext import ndb
+
+
+
+def computeEventId(hook):
+    abi = hook.abi
+    assert abi['type'] == 'event'
+    return abi.method_id(abi['name'], [x['type'] for x in abi['inputs']])
 
 
 class Hook(ndb.Model):
     address = ndb.BlobProperty(indexed=True)                # Address of the contract sending the event
-    topic0 = ndb.BlobProperty(indexed=True, required=True)  # First log topic, usually event ID
-    topics = ndb.BlobProperty(indexed=False, repeated=True) # List of log topics to filter on
-    abi = ndb.JsonProperty(indexed=False)                   # ABI for event
+    abi = ndb.JsonProperty(indexed=False, required=True)    # ABI for event
+    eventId = ndb.ComputedProperty(computeEventId)          # Event ID to filter for
     uri = ndb.StringProperty(indexed=False)                 # URI (possibly templated) to query
     body = ndb.JsonProperty(indexed=False)                  # Body of POST request
     contentType = ndb.StringProperty(indexed=False)         # Content-type of body
